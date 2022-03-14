b7f8e8d427ba382f2302e6b8c46af7f7c24b4b0c
Alex Zybert <alex_zybert@hotmail.com>
2022-03-15 00:48:43 +0100

    checked all possible errors and added logger

--- src/rover/Mars.java
+++ src/rover/Mars.java
@@ -4,70 +4,98 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Mars {
 
-	public static int maxX;
-	public static int maxY;
-
+	private int maxX;
+	private int maxY;
 	
-	static ArrayList<Rover> Landing(String fileName) {
-		ArrayList<Rover> rovers = new ArrayList<Rover>();
+	private static Logger myLogger = Logger.getLogger(Mars.class.toString());
+	
+	public int getMaxY() {
+		return maxY;
+	}
+
+	private void setMax(int maxX, int maxY) {
+		this.maxX = maxX;
+		this.maxY = maxY;
+	}
+
+	public int getMaxX() {
+		return maxX;
+	}
+
+
+	public ArrayList<Rover> land(String landingData) throws Exception {
+		ArrayList<Rover> rovers = new ArrayList<>();
 
 		try {
-			File myObj = new File(fileName);
+			File myObj = new File(landingData);
 			Scanner myReader = new Scanner(myObj);
 			
 			if (myReader.hasNextLine()) {
 				String data[] = myReader.nextLine().split(" ");
-				maxX = Integer.parseInt(data[0]);
-				maxY = Integer.parseInt(data[1]);
+				if (data.length != 2)
+					throw new Exception("Bondaries of the map badly defined");
+				setMax(Integer.parseInt(data[0]), Integer.parseInt(data[1]));
 			}
 			
 			while (myReader.hasNextLine()) {
 				String data[] = myReader.nextLine().split(" ");
-				Rover rover = new Rover(Integer.parseInt(data[0]), Integer.parseInt(data[1]), data[2]);
+				if (data.length != 3 || (data[2].charAt(0) != 'N' && data[2].charAt(0) != 'E' && data[2].charAt(0) != 'W' && data[2].charAt(0) != 'S'))
+					throw new Exception("Something wrong about the landing data of a rover");
+				Rover rover = new Rover(Integer.parseInt(data[0]), Integer.parseInt(data[1]), data[2].charAt(0));
 				if (myReader.hasNextLine())
 					rover.setOrders(myReader.nextLine());
-				else 
-					rover.setOrders(null);
+				else
+					rover.setOrders("");
 				rovers.add(rover);
 			}
 			
 			myReader.close();
-			
 		} catch (FileNotFoundException e) {
-			System.out.println("There's something wrong with your file, buddy!");
-			e.printStackTrace();
+			myLogger.log(Level.SEVERE, "There's something wrong with your file: [0] ", e.getMessage());
 		}
-
 		return rovers;
 	}
 	
 	
-	static void Exploring(ArrayList<Rover> rovers)
+	
+	public void explore(ArrayList<Rover> rovers)
 	{
 		for (Rover r : rovers) {
-			r.doOrders();
+			r.doOrders(this.getMaxX(),this.getMaxY());
 		}
 	}
 	
 	
-	static void SendingData(ArrayList<Rover> rovers)
+	
+	public void sendData(ArrayList<Rover> rovers)
 	{
+		//myLogger.setLevel(Level.FINEST);
+		//ConsoleHandler handler = new ConsoleHandler();
+        //handler.setLevel(Level.FINEST);
+        //myLogger.addHandler(handler);
 		for (Rover r : rovers) {
+			//myLogger.log(Level.FINEST, r.toString());
 			System.out.println(r);
 		}
 	}
 	
-
-	public static void main(String[] args) throws Exception {
-		if (args.length < 1)
+	
+	
+	public void LaunchSpaceship(String[] landingData) throws Exception
+	{
+		if (landingData.length < 1)
 			throw new Exception("Please, type the name of the file to read as first parameter");
-
-		ArrayList<Rover> rovers = Landing(args[0]);
-		Exploring(rovers);
-		SendingData(rovers);
+		
+		ArrayList<Rover> rovers = land(landingData[0]);
+		explore(rovers);
+		sendData(rovers);
 	}
+	
 }
--- src/rover/Rover.java
+++ src/rover/Rover.java
@@ -3,7 +3,7 @@ package rover;
 public class Rover {
 	private int x;
 	private int y;
-	private String orientation;
+	private char orientation;
 	private String orders;
 
 	private final String ORIENTATION_STRING = "NESW";
@@ -11,80 +11,52 @@ public class Rover {
 	public int getX() {
 		return x;
 	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
+	
 	public int getY() {
 		return y;
 	}
 
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public String getOrientation() {
+	public char getOrientation() {
 		return orientation;
 	}
 
-	public void setOrientation(String orientation) {
-		this.orientation = orientation;
-	}
-
-	public String getOrders() {
-		return orders;
-	}
-
-	public void setOrders(String orders) {
+	protected void setOrders(String orders) {
 		this.orders = orders;
 	}
 
-	public Rover(int x, int y, String orientation) {
+	protected Rover(int x, int y, char orientation) {
 		this.x = x;
 		this.y = y;
 		this.orientation = orientation;
 	}
 
-	private void move() {
-		int oldX = this.x;
-		int oldY = this.y;
-
-		if (this.orientation.charAt(0) == 'N')
-			setY(this.y + 1);
-		else if (this.orientation.charAt(0) == 'S')
-			setY(this.y - 1);
-		else if (this.orientation.charAt(0) == 'E')
-			setX(this.x + 1);
-		else if (this.orientation.charAt(0) == 'W')
-			setX(this.x - 1);
-
-		if (this.x > Mars.maxX || this.x < 0 || this.y > Mars.maxY || this.y < 0) {
-			this.x = oldX;
-			this.y = oldY;
-		}
+	private void move(int maxX, int maxY) {
+		if (this.orientation == 'N' && this.y < maxY)
+			this.y = this.y + 1;
+		else if (this.orientation == 'S' && this.y > 0)
+			this.y = this.y - 1;
+		else if (this.orientation == 'E' && this.x < maxX)
+			this.x = this.x + 1;
+		else if (this.orientation == 'W' && this.x > 0)
+			this.x = this.x - 1;
 	}
 
 	private void rotateR() {
 		int i = ORIENTATION_STRING.indexOf(this.orientation);
-		this.setOrientation(ORIENTATION_STRING.charAt((i + 1) % 4));
-	}
-
-	private void setOrientation(char orientation) {
-		this.orientation = String.valueOf(orientation);
+		this.orientation = ORIENTATION_STRING.charAt((i + 1) % 4);
 	}
 
 	private void rotateL() {
 		int i = ORIENTATION_STRING.indexOf(this.orientation);
 		if (i == 0)
 			i = 4;
-		this.setOrientation(ORIENTATION_STRING.charAt((i - 1) % 4));
+		this.orientation = ORIENTATION_STRING.charAt((i - 1) % 4);
 	}
 
-	public void doOrders() {
+	public void doOrders(int maxX, int maxY) {
 		for (int i = 0; i < this.orders.length(); i++) {
 			if (this.orders.charAt(i) == 'M')
-				this.move();
+				this.move(maxX, maxY);
 			if (this.orders.charAt(i) == 'R')
 				this.rotateR();
 			if (this.orders.charAt(i) == 'L')
